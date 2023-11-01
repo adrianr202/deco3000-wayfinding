d388a8aada440964a52e30f25521f33b5338aefd
Adrian Ramos <adrianjosramos202@gmail.com>
2023-11-01 16:31:49 +1100

    update wilkinson_database.py
    changed strings into lower case, changed "descriptions" to "description"

--- wilkinson_database/database.py
+++ wilkinson_database/database.py
@@ -3,22 +3,30 @@
 # Differentiate Study, Class and Lecture
 purpose_database = {
     "Study": {
-        "descriptions": ["Quiet, individual study in a dedicated space.", "A peaceful environment for focused learning."],
-        "keywords": ["study", "revise", "quiet", "individual"]
+        "name": ["Masters Homebase"],
+        "capacity": 50,
+        "description": ["Master Homebase is your place to study or take a break."],
+        "keywords": ["study", "revise", "quiet", "break"]
     },
     "Class": {
-        "descriptions": ["Structured classroom activities.", "Engage with your peers in a learning environment."],
+        "name": ["Seminar Room 241"],
+        "description": ["Class for DECO3000 is located in Seminar Room 241"],
         "keywords": ["class", "learning", "peer", "studio"]
     },
     "Lecture": {
-        "descriptions": ["Attend a lecture or seminar.", "Listen to a guest speaker on a specific topic."],
+        "name": ["Lecture Room 270"],
+        "capacity": 150,
+        "description": ["Your lecture will be at Lecture Room 270."],
         "keywords": ["lecture", "seminar", "guest speaker"]
     },
     "Gallery Visit": {
-        "descriptions": ["Space for art, paintings and sculptures open for viewing from Architecture and Design students."],
-        "keywords": ["musuem", "artworks", "gallery"]
+        "name": ["Tin Sheds Gallery"],
+        "capacity": 50,
+        "description": ["Gallery in Wilkinson Building offering art works featured in Architecture and Design"],
+        "keywords": ["museum", "artworks", "gallery"]
     },
     "Unknown Purpose": {
+        "name": ["Unknown"],
         "descriptions": ["I'm not sure about your specific purpose in Wilkinson Building."],
         "keywords": []
     },
@@ -27,27 +35,31 @@ purpose_database = {
 room_database = {
     # Tin Sheds Gallery 
     "gallery": {
-        "name": "Tin Sheds Gallery",
+        "name": "tin sheds gallery",
         "capacity": 50,
-        "description": "Gallery offering art works featured in Architecture and Design."
+        "description": "Gallery in Wilkinson Building offering art works featured in Architecture and Design.",
+
     },
     # Lecture
     "270": {
-        "name": "Lecture Room 270",
+        "name": "lecture room 270",
         "capacity": 150,
-        "description": "A lecture room for educational presentations with seating, audio-visual equipment and suitable lighting."
+        "description": "A lecture room for educational presentations with seating, audio-visual equipment and suitable lighting.",
+
     },
     # Class
     "241": {
-        "name": "Seminar Room 241",
+        "name": "seminar room 241",
         "capacity": 30,
-        "description": "A studio room for small, interactive group discussions and studying."
+        "description": "A studio room for small, interactive group discussions and collaborative learning.",
+
     },
     # Study
     "homebase": {
-        "name": "Masters Homebase",
+        "name": "masters homebase",
         "capacity": 50,
-        "description": "A room with equiped with computers and spots for model making. Great area for quite study."
+        "description": "A room with equiped with computers and spots for model making. Great area for quite study.",
+
     },
 }
 
